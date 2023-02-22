99094b4667a3036a98761567245d4cc026c459fe
Zamora, Marco <marco.zamora@frostbank.com>
2023-02-22 14:37:43 -0600

    Fresh Project 02/22/2023

---
+++ How Much/How Much/How_MuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  How_MuchApp.swift
+//  How Much
+//
+//  Created by ma22756 on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct How_MuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
